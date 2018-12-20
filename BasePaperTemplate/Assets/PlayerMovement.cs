ae5a9bbfec57cdb936b9191c84330b8b3a0cef05
PaperCraftGameDev <kachekgamedev@gmail.com>
2018-12-20 15:28:52 -0600

    Add Movement Script to Player

---
+++ BasePaperTemplate/Assets/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    //variables
+    public float speed = .1f;
+    private Rigidbody rb;
+    //private CharacterController cc;
+    private Vector3 movement;
+    public float frontOfStage = -9.0f;
+
+    private void Start()
+    {
+        //initialize variables with components
+        rb = GetComponent<Rigidbody>();
+        //cc = GetComponent<CharacterController>();
+    }
+
+    private void FixedUpdate()
+    {
+        //get movement input
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        //create a vector from input
+        movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        //modifications to the vector based on other logic
+
+        //Don't move too far forward
+        if (rb.position.z <= frontOfStage && movement.z < 0)
+            movement.z = 0;
+
+        //Apply movement
+        rb.MovePosition(rb.position + movement * speed);
+        //cc.Move(movement * speed);
+        //rb.AddForce(movement * speed);
+    }
+}
