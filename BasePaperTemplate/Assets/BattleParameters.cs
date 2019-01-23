8098be7903bdc0cfd4ec6aece75f504f83d2b90e
PaperCraftGameDev <kachekgamedev@gmail.com>
2019-01-23 16:33:24 -0600

    Wander script and across scene persistance
    Script for wandering around. Also has the basics of across scene persistance and and outline for enemy collisions.

---
+++ BasePaperTemplate/Assets/BattleParameters.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleParameters : MonoBehaviour {
+
+    public static BattleParameters battleParameters;
+
+    //put savable parameters here
+
+    private void Awake()
+    {
+        if(battleParameters == null)
+        {
+            battleParameters = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if(battleParameters != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
