ebb8b38c0b96027cc79917a9bc6ef49ce7a8503a
PaperCraftGameDev <kachekgamedev@gmail.com>
2019-02-01 14:22:43 -0600

    Basic Dialogue Components
    Dialogue set up with text animation.
    Does not include dialogue box in/out animations.

---
+++ BasePaperTemplate/Assets/Dialogue.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class Dialogue {
+
+    [TextArea(3,3)]
+    public string[] sentences;
+	
+}
---
+++ BasePaperTemplate/Assets/PlayerInteraction.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour {
+
+    private bool isTalking = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.name);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.name == "NPC" && Input.GetKeyDown("a"))
+        {
+            if(isTalking)
+            {
+                FindObjectOfType<DialogueManager>().DisplayNextSentence();
+            }
+            else
+            {
+                Debug.Log("Talking");
+                isTalking = true;
+                other.gameObject.GetComponent<DialogueTrigger>().TriggerDialogue();
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        isTalking = false;
+    }
+
+}
